50461c45bdf8145cde7cd884d54be8d76750cd5f
Ryan Lam <rwl@alumni.cmu.edu>
2017-10-14 18:31:28 -0400

    Remove x64 folder

--- code/x64/Release/memcheck.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cuda-memcheck SilkRAD.exe "..\..\vmf\%1"
-copy out.bsp D:\Steam\steamapps\sourcemods\BMS\maps\
--- code/x64/Release/test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-SilkRAD.exe "..\..\vmf\%1"
-copy out.bsp "C:\Program Files (x86)\Steam\steamapps\sourcemods\BMS\maps\"
-REM copy out.bsp "D:\Steam\steamapps\sourcemods\BMS\maps\"
